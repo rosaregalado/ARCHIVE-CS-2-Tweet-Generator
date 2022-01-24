d11e7e41f19c91f1106c875f18c0509424c4eb1c
Rosa Regalado <rosaregalado101@gmail.com>
2022-01-24 12:28:27 -0800

    random dictionary words

--- dictionary_words.py
+++ dictionary_words.py
@@ -1,10 +1,20 @@
 import random
 
 def dictionary_words():
+  words_list = []
+  output = ""
+  # actual amount of words: 235886
+  total_words = 20 
   open_file = open('/usr/share/dict/words')
-  file_words = open_file.readlines()
-  num_words = random.randint(0, len(file_words)-1)
+  words = open_file.readlines()
+  # generate random number (of words)
+  for i in range(total_words):
+    random_number = random.randint(0, len(words)-1)
+    words_list.append(words[random_number])
 
+  for i in words_list:
+    output += i
+  print(output)
 
 if __name__ == "__main__":
   dictionary_words()
